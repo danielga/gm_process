6cf5d2635b241ff1dad95a17c8a1c710c49f0160
Daniel <danielga@outlook.pt>
2015-06-25 17:45:58 +0100

    Converted to garrysmod_common. Removed gmod-module-base submodule. Updated readme. Added license.

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -1,100 +1,23 @@
-GARRYSMOD_MODULE_BASE_FOLDER = "../gmod-module-base"
-SOURCE_FOLDER = "../source"
-LIBEXECSTREAM_FOLDER = "../libexecstream"
-PROJECT_FOLDER = os.get() .. "/" .. _ACTION
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://bitbucket.org/danielga/garrysmod_common) directory",
+	value = "path to garrysmod_common dir"
+})
 
-solution("gm_process")
-	language("C++")
-	location(PROJECT_FOLDER)
-	warnings("Extra")
-	flags({"NoPCH", "StaticRuntime"})
-	platforms({"x86"})
-	configurations({"Release", "Debug"})
+local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
+if gmcommon == nil then
+	error("you didn't provide a path to your garrysmod_common (https://bitbucket.org/danielga/garrysmod_common) directory")
+end
 
-	filter("platforms:x86")
-		architecture("x32")
+include(gmcommon)
 
-	filter("configurations:Release")
-		optimize("On")
-		vectorextensions("SSE2")
-		objdir(PROJECT_FOLDER .. "/intermediate")
-		targetdir(PROJECT_FOLDER .. "/release")
+local LIBEXECSTREAM_FOLDER = "../libexecstream"
 
-	filter("configurations:Debug")
-		flags({"Symbols"})
-		objdir(PROJECT_FOLDER .. "/intermediate")
-		targetdir(PROJECT_FOLDER .. "/debug")
+CreateSolution("luaerror")
+	CreateProject(SERVERSIDE)
+		includedirs({LIBEXECSTREAM_FOLDER})
+		files({LIBEXECSTREAM_FOLDER .. "/exec-stream.cpp"})
 
-	project("gmsv_process")
-		kind("SharedLib")
-		defines({"GMMODULE", "PROCESS_SERVER"})
-		includedirs({
-			SOURCE_FOLDER,
-			GARRYSMOD_MODULE_BASE_FOLDER .. "/include",
-			LIBEXECSTREAM_FOLDER
-		})
-		files({
-			SOURCE_FOLDER .. "/*.cpp",
-			SOURCE_FOLDER .. "/*.hpp",
-			LIBEXECSTREAM_FOLDER .. "/exec-stream.cpp",
-			LIBEXECSTREAM_FOLDER .. "/exec-stream.h"
-		})
-		vpaths({
-			["Header files"] = {
-				SOURCE_FOLDER .. "/**.hpp",
-				LIBEXECSTREAM_FOLDER .. "/**.h"
-			},
-			["Source files"] = {
-				SOURCE_FOLDER .. "/**.cpp",
-				LIBEXECSTREAM_FOLDER .. "/**.cpp"
-			}
-		})
-
-		targetprefix("")
-		targetextension(".dll")
-
-		filter("system:windows")
-			targetsuffix("_win32")
-
-		filter("system:linux")
-			targetsuffix("_linux")
-
-		filter({"system:macosx"})
-			targetsuffix("_mac")
-
-	project("gmcl_process")
-		kind("SharedLib")
-		defines({"GMMODULE", "PROCESS_CLIENT"})
-		includedirs({
-			SOURCE_FOLDER,
-			GARRYSMOD_MODULE_BASE_FOLDER .. "/include",
-			LIBEXECSTREAM_FOLDER
-		})
-		files({
-			SOURCE_FOLDER .. "/*.cpp",
-			SOURCE_FOLDER .. "/*.hpp",
-			LIBEXECSTREAM_FOLDER .. "/exec-stream.cpp",
-			LIBEXECSTREAM_FOLDER .. "/exec-stream.h"
-		})
-		vpaths({
-			["Header files"] = {
-				SOURCE_FOLDER .. "/**.hpp",
-				LIBEXECSTREAM_FOLDER .. "/**.h"
-			},
-			["Source files"] = {
-				SOURCE_FOLDER .. "/**.cpp",
-				LIBEXECSTREAM_FOLDER .. "/**.cpp"
-			}
-		})
-
-		targetprefix("")
-		targetextension(".dll")
-
-		filter("system:windows")
-			targetsuffix("_win32")
-
-		filter("system:linux")
-			targetsuffix("_linux")
-
-		filter({"system:macosx"})
-			targetsuffix("_mac")
+	CreateProject(CLIENTSIDE)
+		includedirs({LIBEXECSTREAM_FOLDER})
+		files({LIBEXECSTREAM_FOLDER .. "/exec-stream.cpp"})
